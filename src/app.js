c69df3ed190171bd9e8dc2c17fe849710722d2db
Metodi Bozhkov <mlbozhkov@gmail.com>
2022-09-14 01:41:39 +0300

    Restructure code & loop over Records
    Restructure code and remove "magical" variable names. Export csvNameParams from util.js containing proper variables to access Row Data from CSV.
    
    Start Iterating CSV Row Data and begin extraction/scraping logic by making Object holding Date as Key and USD Rate as Value.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import { getMetadataForFileList } from './util/file_util.js';
-import { csvToArray } from './util/util.js';
+import { csvToArray, csvNameParams } from './util/util.js';
 import { getConversionRateByDate } from './api/data.js';
 
 window.addEventListener(
@@ -22,7 +22,7 @@ window.addEventListener(
 let csvFile = document.getElementById('fileCSV');
 let labelFile = document.querySelector('.dragNdrop label');
 let fileName = document.querySelector('.dragNdrop p');
-let content = document.querySelector('.content');
+let content = document.querySelector('.preview');
 let btnStart = document.getElementById('startBtn');
 
 btnStart.addEventListener('click', requestDataHandler);
@@ -51,7 +51,7 @@ function previewFile(e) {
 
     reader.addEventListener('load', () => {
       content.innerText = reader.result;
-      records = [...csvToArray(reader.result)];
+      records = csvToArray(reader.result);
     });
 
     reader.readAsText(file);
@@ -72,27 +72,69 @@ dropArea.addEventListener('dragover', (e) => {
 dropArea.addEventListener('drop', (e) => previewFile(e));
 
 async function requestDataHandler(event) {
+  // Insert Loop Logic with using records[n] for every record. Use an Object an push Dates with Rates. If Date already exists then Don't make an API GET Request !
+
   if (!records[0]) {
     return;
   }
-  let selectedDate = new Date(records[0].acquiredDate);
-  let dateObj = {
-    day: selectedDate.getDate(),
-    month: (selectedDate.getMonth()+1).toLocaleString('en-US', {
-      minimumIntegerDigits: 2,
-      useGrouping: false,
-    }),
-    year: selectedDate.getFullYear(),
-  };
-console.log(dateObj);
-  let result = await getConversionRateByDate(dateObj);
-  console.log(result);
+
   let resultHTMLElement = document.createElement('body');
-  
-  resultHTMLElement.innerHTML = result;
-  console.log(resultHTMLElement);
-  let currentTable = resultHTMLElement.querySelectorAll('.table_box tbody tr');
-  console.log(currentTable);
-  content.innerText = '';
-  currentTable.forEach(r=>content.innerText+=r.innerHTML);
+
+  let accumulatedDatesPreview = document.createElement('div');
+
+  let DatesRecords = {};
+  let result;
+  // Use Loop to Extract Rate by Acquired OR Sold Dates and Immediately Calculate the Conversion... 
+  // After that store the result for the current Row Transaction the Converted Values for the Acquired Amount and Sold Amount by the Date
+  for (let idx = 0; idx < records.length; idx++) {
+    const curRecord = records[idx];
+    let acquiredDateRecord = curRecord[csvNameParams.acquiredDate];
+    let soldDateRecord = curRecord[csvNameParams.soldDate];
+    console.log(curRecord);
+
+    if (DatesRecords?.acquiredDateRecord === undefined) {
+
+      // Extract this code of block to reuse it for acquiredDate and soldDate
+      let selectedDate = new Date(curRecord[csvNameParams.acquiredDate]);
+      let dateObj = {
+        day: selectedDate.getDate(),
+        month: (selectedDate.getMonth() + 1).toLocaleString('en-US', {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        }),
+        year: selectedDate.getFullYear(),
+      };
+      console.log(dateObj);
+      result = await getConversionRateByDate(dateObj);
+      //console.log(result);
+      resultHTMLElement.innerHTML = result;
+      //console.log(resultHTMLElement);
+      let currentTable = resultHTMLElement.querySelector('.table_box tbody');
+      let USDrow = currentTable.querySelector(
+        'tbody>tr:nth-child(30)>td:nth-child(2)'
+      ).parentElement;
+      let USD_BGN_Rate = Number(
+        USDrow.querySelector('td:nth-child(4)').innerText
+      );
+      console.log(USD_BGN_Rate);
+      console.log(currentTable);
+      accumulatedDatesPreview.innerText += `Date - ${csvNameParams.acquiredDate} : USD Rate - ${USD_BGN_Rate}`;
+
+      DatesRecords[acquiredDateRecord] = USD_BGN_Rate;
+      ///
+
+
+    } 
+    if (acquiredDateRecord!==soldDateRecord && DatesRecords?.soldDateRecord === undefined) {
+      // reuse logic from acquiredDate code block above;
+    }
+    
+    
+    
+      // If date is already had for this Stock Transaction Record Row then just Do the Maths and Store result in converted Units
+    
+  }
+  content.innerHTML = '';
+
+  content.appendChild(accumulatedDatesPreview);
 }
--- src/util/util.js
+++ src/util/util.js
@@ -1,8 +1,11 @@
+let csvNameParams;
+
 function csvToArray(str, delimiter = ',') {
   const records = [];
   // slice from start of text to the first \n index
   // use split to create an array from string by delimiter
-  const [
+
+  const  [
     acquiredDate,
     soldDate,
     Symbol,
@@ -10,31 +13,41 @@ function csvToArray(str, delimiter = ',') {
     CostBasis,
     Amount,
     RealisedPnL,
-  ] = str.slice(0, str.indexOf('\n')).split(delimiter);
+   ] =  str.slice(0, str.indexOf('\n')).split(delimiter);
 
+   csvNameParams={ "acquiredDate":acquiredDate,
+    "soldDate":soldDate,
+    "Symbol":Symbol,
+    "Quantity":Quantity,
+    "CostBasis":CostBasis,
+    "Amount":Amount,
+    "RealisedPnL":RealisedPnL};
   // slice from \n index + 1 to the end of the text
   // use split to create an array of each csv value row
-  const rows = str.slice(str.indexOf('\n') + 1).split('\n');
+  const rows = str
+    .slice(str.indexOf('\n') + 1)
+    .split('\n')
+    .filter((x) => x !== '');
 
   for (const row of rows) {
     let [
-      acquiredDate,
-      soldDate,
-      Symbol,
-      Quantity,
-      CostBasis,
-      Amount,
-      RealisedPnL,
+      acquiredDateVal,
+      soldDateVal,
+      SymbolVal,
+      QuantityVal,
+      CostBasisVal,
+      AmountVal,
+      RealisedPnLVal,
     ] = row.split(delimiter);
 
     let recordObj = {
-      acquiredDate,
-      soldDate,
-      Symbol,
-      Quantity,
-      CostBasis,
-      Amount,
-      RealisedPnL,
+      [csvNameParams.acquiredDate]: acquiredDateVal,
+      [csvNameParams.soldDate]: soldDateVal,
+      [csvNameParams.Symbol]: SymbolVal,
+      [csvNameParams.Quantity]: QuantityVal,
+      [csvNameParams.CostBasis]: CostBasisVal,
+      [csvNameParams.Amount]: AmountVal,
+      [csvNameParams.RealisedPnL]: RealisedPnLVal,
     };
 
     records.push(recordObj);
@@ -45,4 +58,4 @@ function csvToArray(str, delimiter = ',') {
   return records;
 }
 
-export { csvToArray };
+export { csvToArray, csvNameParams };
